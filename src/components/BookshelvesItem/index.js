0c430286c6966f1a19dfac90910713fe9dac6abf
Pavan Kumar <119096078+PavanPk1@users.noreply.github.com>
2023-10-06 21:55:59 +0530

    Update index.js

--- src/components/BookshelvesItem/index.js
+++ src/components/BookshelvesItem/index.js
@@ -8,23 +8,23 @@ const BookshelvesItem = props => {
   //    console.log(bookDetails)
   const {id, coverPic, authorName, rating, readStatus, title} = bookDetails
   return (
-    <Link to={`/books/${id}`} className="link">
-      <li className="bookContainer">
-        <img src={coverPic} alt={title} className="coverPic" />
-        <div className="bookDetailsContainer">
-          <h1 className="bookName">{title}</h1>
-          <p className="authorName">{authorName}</p>
-          <div className="ratingContainer">
-            <p className="rating">Avg Rating</p>
-            <BsFillStarFill className="ratingStar" />
-            <p className="rating">{rating}</p>
+    <li className="bookContainer">
+      <Link to={`/books/${id}`} className="link">
+          <img src={coverPic} alt={title} className="coverPic" />
+          <div className="bookDetailsContainer">
+            <h1 className="bookName">{title}</h1>
+            <p className="authorName">{authorName}</p>
+            <div className="ratingContainer">
+              <p className="rating">Avg Rating</p>
+              <BsFillStarFill className="ratingStar" />
+              <p className="rating">{rating}</p>
+            </div>
+            <p className="statusTitle">
+              Status: <span className="readStatus">{readStatus}</span>
+            </p>
           </div>
-          <p className="statusTitle">
-            Status: <span className="readStatus">{readStatus}</span>
-          </p>
-        </div>
-      </li>
-    </Link>
+      </Link>
+   </li>
   )
 }
 
